75991d164d5c621f4e8dd42942c38f1818fed5bc
newrodrigo <rodrigocerqueira@outlook.com>
2023-02-17 22:12:14 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -6,7 +6,6 @@ import com.devsuperior.dscatalog.entities.Category;
 
 public class CategoryDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
 
 	private Long id;
 	private String name;
@@ -40,6 +39,4 @@ public class CategoryDTO implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
 }
